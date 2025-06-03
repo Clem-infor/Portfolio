c1462d37ba547344e9b236275b4687767cd9fd48
Clem-infor <clement.harinquet@gmail.com>
2025-06-03 12:11:54 +0200

    Mise à jour de la page stage 03/06 js fond

--- script.js
+++ script.js
@@ -66,3 +66,66 @@ window.addEventListener("load", () => {
     observer.observe(el);
   });
   
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 40,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#00ff99"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false
+      },
+      "size": {
+        "value": 3,
+        "random": true
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out"
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#00ff99",
+        "opacity": 0.3,
+        "width": 1
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": false
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 0.4
+          }
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
